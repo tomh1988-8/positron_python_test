d77c91dc82c3fc9b8ffc0616b87a3326501c7901
tom.hunter <tom.hunter@cityandguilds.com>
2025-01-10 12:52:22 +0000

    extended functionality to play around with Codeium

--- scripts/my-r-project/app.R
+++ scripts/my-r-project/app.R
@@ -7,7 +7,8 @@ library(shiny)
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins", "Number of bins:", min = 1, max = 50, value = 30)
+      sliderInput("bins", "Number of bins:", min = 1, max = 50, value = 30),
+      checkboxInput("show_density", "Show density curve")
     ),
     mainPanel(
       plotOutput("distPlot")
@@ -20,14 +21,17 @@ server <- function(input, output) {
   output$distPlot <- renderPlot({
     x <- faithful[, 2] # Old Faithful Geyser data
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    hist(x, breaks = bins, col = "#75AADB", border = "white")
+    hist(x,
+      breaks = bins, col = "#75AADB", border = "white",
+      probability = TRUE,
+      main = "Histogram of Old Faithful Geyser data"
+    )
+    if (input$show_density) {
+      dens <- density(x)
+      lines(dens$x, dens$y, col = "red")
+    }
   })
 }
 
 # Run the application
-# * comment
-# ! comment
-# ? comment
-# TODO: comment
-
 shinyApp(ui = ui, server = server)
